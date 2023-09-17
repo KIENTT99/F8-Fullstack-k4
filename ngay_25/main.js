c632d9a8164423d19b0c096db7c26f3984c92961
KIENTT99 <trungkienttk9x@gmail.com>
2023-09-17 15:04:39 +0900

    bài 25

--- ngay_25/main.js
+++ ngay_25/main.js
@@ -29,3 +29,55 @@ carouselPrevBtn.addEventListener("click", function () {
     position += itemWidth;
     carouselInner.style.translate = `${position}px`;
 });
+
+//slide
+var isDown = false;
+var slide = (10 * itemWidth) / 100;
+var starOff;
+var move;
+
+carouselInner.addEventListener("mousedown", function (e) {
+    e.preventDefault();
+    isDown = true;
+    starOff = e.offsetX;
+});
+
+carouselInner.addEventListener("mousemove", function (e) {
+    e.preventDefault();
+    if (isDown) {
+        move = e.offsetX - starOff;
+        carouselInner.style.cursor = "move";
+        if (move < 0) {
+            if (
+                Math.abs(move) > slide &&
+                Math.abs(position) < totalWidth - itemWidth
+            ) {
+                position -= itemWidth;
+                carouselInner.style.translate = `${position + move}px`;
+                isDown = false;
+                index++;
+                activeDots();
+            } else {
+                carouselInner.style.translate = `${position + move}px`;
+            }
+        } else if (move > 0) {
+            if (position < 0 && move > slide) {
+                position += itemWidth;
+                carouselInner.style.translate = `${position + move}px`;
+                isDown = false;
+                index--;
+                activeDots();
+            } else {
+                carouselInner.style.translate = `${position + move}px`;
+            }
+        }
+    }
+});
+
+carouselInner.addEventListener("mouseup", function (e) {
+    e.preventDefault();
+    isDown = false;
+    carouselInner.style.cursor = "default";
+    carouselInner.style.translate = `${position}px`;
+    console.log(position);
+});
